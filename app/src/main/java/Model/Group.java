32479a5fda364bdd6b6c2d94d34b7a48aba6dc30
Mateus <mateushnr.obs@gmail.com>
2024-03-19 16:00:30 -0300

    feat: add my groups layout and list implementation from static data

---
+++ app/src/main/java/Model/Group.java
@@ -0,0 +1,37 @@
+package Model;
+
+public class Group {
+    private int idGroup;
+    private String name;
+    private String description;
+
+    public Group(int idGroup, String name, String description) {
+        this.idGroup = idGroup;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getIdGroup() {
+        return idGroup;
+    }
+
+    public void setIdGroup(int idGroup) {
+        this.idGroup = idGroup;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
